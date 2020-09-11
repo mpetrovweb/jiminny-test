482014e9afd75782bd8fb9e7d671e43cee71d94f
mpetrov <mpetrovweb@gmail.com>
2020-09-11 22:28:16 +0300

    Add initial setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="app">
+		<Journeys />
+	</div>
+</template>
+
+<script>
+import Journeys from '@/components/Journeys';
+
+export default {
+	name: 'App',
+
+	components: {
+		Journeys
+	}
+}
+</script>
+
+<style lang="scss">
+	@import '@/assets/scss/main';
+</style>
---
+++ src/components/Journeys.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="journeys">
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Journeys',
+
+	data () {
+		return {
+
+		}
+	},
+
+	mounted() {
+		fetch('http://localhost:8080/journeys.json')
+			.then(response => response.json())
+			.then(data => console.log(data));
+	}
+}
+</script>
